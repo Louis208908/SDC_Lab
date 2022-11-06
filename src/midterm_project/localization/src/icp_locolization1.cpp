17a38f947e3d2adce82d99b066033f33bf873561
louis208908 <lewis8908@gmail.com>
2022-11-06 23:54:01 +0800

    final submission in Competition 1 by icp1.launch

---
+++ src/midterm_project/localization/src/icp_locolization1.cpp
@@ -0,0 +1,278 @@
+#include "math.h"
+#include <string>
+#include "stdio.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <std_msgs/String.h>
+#include "sensor_msgs/Imu.h"
+#include <pcl/conversions.h>
+#include <nav_msgs/Odometry.h>
+#include <pcl_ros/transforms.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <pcl/registration/icp.h>
+#include <tf/transform_listener.h>
+#include <pcl/filters/voxel_grid.h>
+#include <tf/transform_datatypes.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/filters/passthrough.h>
+#include <tf/transform_broadcaster.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+
+class icp_localization{
+
+private:
+
+	// =============== ros util parameters ===============
+	ros::NodeHandle nh;
+	ros::Subscriber sub_lidar_scan;
+	tf::TransformListener tf_listener;
+	tf::TransformBroadcaster tf_broadcaster;
+
+
+	// =============== variables of transformation ===============
+	Eigen::Matrix4f initial_guess;
+	pcl::PointCloud<pcl::PointXYZI>::Ptr map;	
+	sensor_msgs::PointCloud2 Final_map;
+	sensor_msgs::PointCloud2 Final_cloud;
+	Eigen::Matrix4f l2c_eigen_transform;
+
+
+	// =============== variables of output file ===============
+	std::string map_path, result_path;
+	std::ofstream outfile;
+
+	// =============== variables of ICP parameters ===============
+	double map_leaf_size;
+	double scan_leaf_size;
+
+public:
+	int frame_number;
+
+	/**
+	 * @brief Construct a new icp localization object, initializing ICP(get initial guess)
+	 * 
+	 * @param _nh ros node handler
+	 */
+	icp_localization(ros::NodeHandle _nh){
+
+		std::vector<float> trans, rot;
+		this->nh = _nh;
+		this->sub_lidar_scan = this->nh.subscribe("/lidar_points", 400000, &icp_localization::lidar_scanning, this);
+		this->frame_number = 0;
+		std::cout << "Initializing ICP...\n";
+
+		// grasping ros parameters
+		_nh.param<std::string>("result_save_path", result_path, "result1.csv");
+		_nh.param<std::string>("map_path", map_path, "itri_map.pcd");
+		_nh.param<double>("scanLeafSize", scan_leaf_size, 0.15);
+		_nh.param<double>("mapLeafSize", map_leaf_size, 0.15);
+		_nh.param<std::vector<float>>("baselink2lidar_trans", trans, std::vector<float>());
+		_nh.param<std::vector<float>>("baselink2lidar_rot", rot, std::vector<float>());
+
+		// 把itri.yaml中的transform link存下來
+		if (trans.size() != 3 | rot.size() != 4)
+			ROS_ERROR("transform not set properly");
+
+		Eigen::Quaternionf link_quaternion(rot.at(3), rot.at(0), rot.at(1), rot.at(2));
+		Eigen::Matrix3f link_rotation = link_quaternion.toRotationMatrix();
+
+		l2c_eigen_transform << 		link_rotation(0, 0), link_rotation(0, 1), link_rotation(0, 2), trans.at(0),
+									link_rotation(1, 0), link_rotation(1, 1), link_rotation(1, 2), trans.at(1),
+									link_rotation(2, 0), link_rotation(2, 1), link_rotation(2, 2), trans.at(2),
+													  0, 		   		   0, 		  			0, 		     1;
+
+		// load map
+		this->map = (new pcl::PointCloud<pcl::PointXYZI>)->makeShared();
+		if (pcl::io::loadPCDFile<pcl::PointXYZI>(map_path, *this->map) == -1) {
+			PCL_ERROR("Couldn't read file map_downsample.pcd \n");
+			exit(0);
+		}
+
+		// getting initial guess
+		this->initial_guess = get_initial_guess();
+		std::cout << "Get initial guess: \n";
+		std::cout << this->initial_guess << std::endl;
+		std::cout << "Ready to localization\n";
+		
+		std::cout << "Result path: " << result_path << std::endl;
+		outfile.open(result_path);
+		outfile << "id,x,y,z,yaw,pitch,roll" << std::endl;
+	}
+
+	/**
+	 * @brief Get the initial guess object 
+	 * 
+	 * @return Eigen::Matrix4f initial guess of map to car transfromation
+	 */
+	Eigen::Matrix4f get_initial_guess(){
+
+		Eigen::Matrix4f initial_guess;
+		geometry_msgs::PointStampedConstPtr gps_point;
+		gps_point = ros::topic::waitForMessage<geometry_msgs::PointStamped>("/gps", this->nh);
+
+		double yaw = 2.449;
+
+		initial_guess << 	cos(yaw),	-sin(yaw),	0,	gps_point->point.x,
+							sin(yaw),	 cos(yaw),	0,	gps_point->point.y,
+								   0,			0,	1,	gps_point->point.z,
+								   0,			0,	0,					 1;
+
+		return initial_guess;
+	}
+
+
+	/**
+	 * @brief Down sampling pointCloud of lidar scan
+	 * 
+	 * @param msg ros topic of lidar scan
+	 * @return pcl::PointCloud<pcl::PointXYZI>::Ptr pointer of PointCloud of lidar scan after downsampling
+	 */
+	pcl::PointCloud<pcl::PointXYZI>::Ptr down_sampling(const sensor_msgs::PointCloud2::ConstPtr &msg){
+
+		pcl::PointCloud<pcl::PointXYZI>::Ptr raw_scan(new pcl::PointCloud<pcl::PointXYZI>);
+		pcl::PointCloud<pcl::PointXYZI>::Ptr result_scan(new pcl::PointCloud<pcl::PointXYZI>);
+		pcl::PCLPointCloud2::Ptr filtered_scan(new pcl::PCLPointCloud2());
+
+		// sensor_msgs -> PointCloud
+		pcl::fromROSMsg(*msg, *raw_scan);
+		// PointCloud -> PointCloud2
+		pcl::toPCLPointCloud2(*raw_scan, *filtered_scan);
+
+		// 可以傳PointCloud 也可以傳PointCloud2, 這邊因為我們傳的是PointCloud2(放在<>裡面的型別)
+		// 因此我們filter的input、output的型別都是PointCloud2
+		// 所以我們上面需要先將msg轉成PointCloud後 再轉乘PointCloud2,
+		pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_filter;
+		voxel_filter.setInputCloud(filtered_scan);
+		voxel_filter.setLeafSize(0.15f, 0.15f, 0.15f);
+		voxel_filter.filter(*filtered_scan);
+
+		// PointCloud2 -> PointCloud
+		pcl::fromPCLPointCloud2(*filtered_scan, *result_scan);
+
+		return result_scan;
+
+	}
+
+
+	/**
+	 * @brief callback fcn when receiving lidar scan, perform icp here
+	 * 
+	 * @param msg ros topic of lidar scan
+	 */
+	void lidar_scanning(const sensor_msgs::PointCloud2::ConstPtr &msg){
+
+		pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_map(new pcl::PointCloud<pcl::PointXYZI>);
+		pcl::PointCloud<pcl::PointXYZI> aligned_points;
+
+		// =============== Passthrough ===============
+		pcl::PassThrough<pcl::PointXYZI> filter;
+		filter.setInputCloud(this->map);
+		filter.setFilterFieldName("x");
+		filter.setFilterLimits(this->initial_guess(0, 3) - 100.0, this->initial_guess(0, 3) + 100.0);
+		filter.filter(*filtered_map);
+
+		filter.setInputCloud(filtered_map);
+		filter.setFilterFieldName("y");
+		filter.setFilterLimits(this->initial_guess(1, 3) - 100.0, this->initial_guess(1, 3) + 100.0);
+		filter.filter(*filtered_map);
+
+		// =============== Down sampling lidar scan ===============
+		pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_scan(new pcl::PointCloud<pcl::PointXYZI>);
+		pcl::PointCloud<pcl::PointXYZI>::Ptr final_filtered_scan(new pcl::PointCloud<pcl::PointXYZI>);
+		filtered_scan = down_sampling(msg);
+
+		// =============== transform scan to car ===============
+		transformPointCloud(*filtered_scan, *filtered_scan, l2c_eigen_transform);
+
+		// =============== start performing ICP ===============
+		pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
+		icp.setInputSource(filtered_scan);
+		icp.setInputTarget(filtered_map);
+		icp.setMaximumIterations(1000);		  			//最大迭代次數
+		icp.setTransformationEpsilon(1e-12);		  	//上次轉換與當前轉換的差值(early stop)
+		icp.setMaxCorrespondenceDistance(1.00);		  	// Distance to see
+		icp.setEuclideanFitnessEpsilon(0.01);		  	//前後兩次迭代誤差的差值(early stop)
+		icp.setRANSACOutlierRejectionThreshold(0.05); 	//距離小於等於inlier_threshold的點對才會被當成是RANSAC的inlier
+		icp.align(aligned_points, this->initial_guess);
+
+		// =============== Get car pos using ICP result===============
+		// initial guess是map 看向 car的轉換
+		this->initial_guess = icp.getFinalTransformation();
+		tf2::Matrix3x3 m2c_trans_rotation;
+		m2c_trans_rotation.setValue(
+			initial_guess(0, 0), initial_guess(0, 1), initial_guess(0, 2),
+			initial_guess(1, 0), initial_guess(1, 1), initial_guess(1, 2),
+			initial_guess(2, 0), initial_guess(2, 1), initial_guess(2, 2)
+		);
+		tf2::Quaternion m2c_rotation_quaternion;
+		m2c_trans_rotation.getRotation(m2c_rotation_quaternion);
+
+		tf::Quaternion m2c_quaternion_matrix(m2c_rotation_quaternion[0], m2c_rotation_quaternion[1], m2c_rotation_quaternion[2], m2c_rotation_quaternion[3]);
+		tf::Matrix3x3 m2c_rotation_angle(m2c_quaternion_matrix);
+		double roll, pitch, yaw;
+		m2c_rotation_angle.getRPY(roll, pitch, yaw);
+
+		std::cout << "Now frame: " << this->frame_number << std::endl;
+		outfile << ++this->frame_number << "," << initial_guess(0, 3) << "," << initial_guess(1, 3) << "," << initial_guess(2, 3) << "," << yaw << "," << pitch << "," << roll << std::endl;
+
+		if (this->frame_number == 201){
+			ROS_INFO("ITRI bag finished");
+		}
+	}
+
+
+	/**
+	 * @brief Destroy the icp localization object
+	 * 
+	 */
+	~icp_localization(){
+		this->outfile.close();
+	}
+
+	/**
+	 * @brief Get the transform between link_name(source) to base_link(target)
+	 * 
+	 * @param link_name source link(lidar)
+	 * @return Eigen::Matrix4f Homogeneous Transformation Matrix
+	 */
+	Eigen::Matrix4f get_transform(std::string link_name){
+		tf::StampedTransform transform;
+		Eigen::Matrix4f eigen_transform;
+
+		try
+		{
+			tf_listener.waitForTransform("base_link", link_name, ros::Time(0), ros::Duration(5.0));
+			tf_listener.lookupTransform("base_link", link_name, ros::Time(0), transform);
+		}
+		catch (tf::TransformException ex)
+		{
+			ROS_ERROR("%s", ex.what());
+			return eigen_transform;
+		}
+		Eigen::Quaternionf q(transform.getRotation().getW(), transform.getRotation().getX(), transform.getRotation().getY(), transform.getRotation().getZ());
+		Eigen::Matrix3f mat = q.toRotationMatrix();
+		eigen_transform << 	mat(0, 0), mat(0, 1), mat(0, 2), transform.getOrigin().getX(),
+							mat(1, 0), mat(1, 1), mat(1, 2), transform.getOrigin().getY(),
+							mat(2, 0), mat(2, 1), mat(2, 2), transform.getOrigin().getZ(),
+									0, 		   0, 		  0, 							1;
+		return eigen_transform;
+	}
+
+};
+
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "icp_locolization");
+	ros::NodeHandle n("~");
+	icp_localization icp_localizer(n);
+	ros::spin();
+
+}
--- src/midterm_project/localization/src/localizer_node.cpp
+++ src/midterm_project/localization/src/localizer_node.cpp
@@ -17,7 +17,8 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 
-class Localizer{
+class Localizer
+{
 
 private:
     float mapLeafSize = 1., scanLeafSize = 1.;
@@ -41,82 +42,85 @@ private:
     std::ofstream outfile;
     geometry_msgs::Transform car2Lidar;
     std::string mapFrame, lidarFrame;
-	double localization_score = 0;
+    double localization_score = 0;
 
 public:
-    Localizer(ros::NodeHandle nh) : map_points(new pcl::PointCloud<pcl::PointXYZI>){
-		std::vector<float> trans, rot;
+    Localizer(ros::NodeHandle nh) : map_points(new pcl::PointCloud<pcl::PointXYZI>)
+    {
+        std::vector<float> trans, rot;
 
-    	_nh = nh;
+        _nh = nh;
 
-		_nh.param<std::vector<float>>("baselink2lidar_trans", trans, std::vector<float>());
-		_nh.param<std::vector<float>>("baselink2lidar_rot", rot, std::vector<float>());
-		_nh.param<std::string>("result_save_path", result_save_path, "result.csv");
-		_nh.param<float>("scanLeafSize", scanLeafSize, 1.0);
-		_nh.param<float>("mapLeafSize", mapLeafSize, 1.0);
-		_nh.param<std::string>("mapFrame", mapFrame, "world");
-		_nh.param<std::string>("lidarFrame", lidarFrame, "nuscenes_lidar");
+        _nh.param<std::vector<float>>("baselink2lidar_trans", trans, std::vector<float>());
+        _nh.param<std::vector<float>>("baselink2lidar_rot", rot, std::vector<float>());
+        _nh.param<std::string>("result_save_path", result_save_path, "result.csv");
+        _nh.param<float>("scanLeafSize", scanLeafSize, 0.3);
+        _nh.param<float>("mapLeafSize", mapLeafSize, 0.3);
+        _nh.param<std::string>("mapFrame", mapFrame, "world");
+        _nh.param<std::string>("lidarFrame", lidarFrame, "nuscenes_lidar");
 
-		ROS_INFO("saving results to %s", result_save_path.c_str());
-		outfile.open(result_save_path);
-		outfile << "id,x,y,z,yaw,pitch,roll" << std::endl;
+        ROS_INFO("saving results to %s", result_save_path.c_str());
+        outfile.open(result_save_path);
+        outfile << "id,x,y,z,yaw,pitch,roll" << std::endl;
 
-		if (trans.size() != 3 | rot.size() != 4)
-			ROS_ERROR("transform not set properly");
-		
+        if (trans.size() != 3 | rot.size() != 4)
+            ROS_ERROR("transform not set properly");
 
         // 把itri.yaml中的transform link存下來
-		car2Lidar.translation.x = trans.at(0);
-		car2Lidar.translation.y = trans.at(1);
-		car2Lidar.translation.z = trans.at(2);
-		car2Lidar.rotation.x = rot.at(0);
-		car2Lidar.rotation.y = rot.at(1);
-		car2Lidar.rotation.z = rot.at(2);
-		car2Lidar.rotation.w = rot.at(3);
+        car2Lidar.translation.x = trans.at(0);
+        car2Lidar.translation.y = trans.at(1);
+        car2Lidar.translation.z = trans.at(2);
+        car2Lidar.rotation.x = rot.at(0);
+        car2Lidar.rotation.y = rot.at(1);
+        car2Lidar.rotation.z = rot.at(2);
+        car2Lidar.rotation.w = rot.at(3);
 
-		sub_map = _nh.subscribe("/map", 1, &Localizer::map_callback, this);
-		sub_points = _nh.subscribe("/lidar_points", 400, &Localizer::pc_callback, this);
-		sub_gps = _nh.subscribe("/gps", 1, &Localizer::gps_callback, this);
-		pub_points = _nh.advertise<sensor_msgs::PointCloud2>("/transformed_points", 1);
-		pub_pose = _nh.advertise<geometry_msgs::PoseStamped>("/lidar_pose", 1);
-		init_guess.setIdentity();
-		ROS_INFO("%s initialized", ros::this_node::getName().c_str());
+        sub_map = _nh.subscribe("/map", 1, &Localizer::map_callback, this);
+        sub_points = _nh.subscribe("/lidar_points", 40000, &Localizer::pc_callback, this);
+        sub_gps = _nh.subscribe("/gps", 1, &Localizer::gps_callback, this);
+        pub_points = _nh.advertise<sensor_msgs::PointCloud2>("/transformed_points", 1);
+        pub_pose = _nh.advertise<geometry_msgs::PoseStamped>("/lidar_pose", 1);
+        init_guess.setIdentity();
+        ROS_INFO("%s initialized", ros::this_node::getName().c_str());
     }
 
     // Gentaly end the node
-    ~Localizer(){
-      if (outfile.is_open()){
-		ROS_INFO("ICP score: %lf", localization_score);
-        outfile.close();
-	  }
+    ~Localizer()
+    {
+        if (outfile.is_open())
+        {
+            ROS_INFO("ICP score: %lf", localization_score);
+            outfile.close();
+        }
     }
 
-
-	/**
-	 * @brief transfer sensor_msgs::PointCloud2 to pcl::pointcloud
-	 * 
-	 * @param msg a rostopic from map publisher using sensor_msgs::PointCloud2
-	 */
-    void map_callback(const sensor_msgs::PointCloud2::ConstPtr &msg){
+    /**
+     * @brief transfer sensor_msgs::PointCloud2 to pcl::pointcloud
+     *
+     * @param msg a rostopic from map publisher using sensor_msgs::PointCloud2
+     */
+    void map_callback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+    {
         // ROS_INFO("Got map message");
         pcl::fromROSMsg(*msg, *map_points);
         map_ready = true;
     }
 
-
     /**
      * @brief callback function when getting a pointCloud
-     * 
+     *
      * @param msg a rostopic of lidar pointCloud
      */
-    void pc_callback(const sensor_msgs::PointCloud2::ConstPtr &msg){
+    void pc_callback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+    {
 
         // ROS_INFO("Got lidar message");
         pcl::PointCloud<pcl::PointXYZI>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZI>);
         Eigen::Matrix4f result;
 
         // 確保map跟gps都收到了
-        while (!(gps_ready & map_ready)){
+        while (!(gps_ready & map_ready))
+        {
             ROS_WARN("waiting for map and gps data ...");
             ros::Duration(0.05).sleep();
             ros::spinOnce();
@@ -173,6 +177,10 @@ public:
         tf::Matrix3x3 mat(q);
         mat.getEulerYPR(yaw, pitch, roll);
         outfile << ++cnt << "," << tf_p.translation().x() << "," << tf_p.translation().y() << "," << tf_p.translation().z() << "," << yaw << "," << pitch << "," << roll << std::endl;
+        if (cnt == 202)
+        {
+            ROS_INFO("ITRI bag finished");
+        }
     }
 
     /**
@@ -180,14 +188,16 @@ public:
      *
      * @param msg a rostopic of gps data
      */
-    void gps_callback(const geometry_msgs::PointStamped::ConstPtr &msg){
+    void gps_callback(const geometry_msgs::PointStamped::ConstPtr &msg)
+    {
 
         // ROS_INFO("Got GPS message");
         gps_point.x = msg->point.x;
         gps_point.y = msg->point.y;
         gps_point.z = msg->point.z;
 
-        if (!initialied){
+        if (!initialied)
+        {
             geometry_msgs::PoseStamped pose;
             pose.header = msg->header;
             pose.pose.position = msg->point;
@@ -206,11 +216,12 @@ public:
 
     /**
      * @brief using ICP to align pointCloud, finding HTM of the car
-     * 
+     *
      * @param scan_points Input PointCloud from lidar data
      * @return Eigen::Matrix4f a HTM representing ICP result
      */
-    Eigen::Matrix4f align_map(const pcl::PointCloud<pcl::PointXYZI>::Ptr scan_points){
+    Eigen::Matrix4f align_map(const pcl::PointCloud<pcl::PointXYZI>::Ptr scan_points)
+    {
 
         pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZI>());
         pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_map_ptr(new pcl::PointCloud<pcl::PointXYZI>());
@@ -218,53 +229,60 @@ public:
         Eigen::Matrix4f result;
 
         /* [Part 1] Perform pointcloud preprocessing here e.g. downsampling use setLeafSize(...) ... */
-		voxel_filter.setLeafSize(mapLeafSize, mapLeafSize, mapLeafSize);
-		voxel_filter.setInputCloud(map_points);
-		voxel_filter.filter(*filtered_map_ptr);
-		voxel_filter.setLeafSize(scanLeafSize, scanLeafSize, scanLeafSize);
-		voxel_filter.setInputCloud(scan_points);
-		voxel_filter.filter(*filtered_scan_ptr);
+        voxel_filter.setLeafSize(mapLeafSize, mapLeafSize, mapLeafSize);
+        voxel_filter.setInputCloud(map_points);
+        voxel_filter.filter(*filtered_map_ptr);
+        voxel_filter.setLeafSize(scanLeafSize, scanLeafSize, scanLeafSize);
+        voxel_filter.setInputCloud(scan_points);
+        voxel_filter.filter(*filtered_scan_ptr);
 
-		/* Find the initial orientation for fist scan */
-        if (!initialied){
+        /* Find the initial orientation for fist scan */
+        if (!initialied)
+        {
             pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> first_icp;
             float yaw, min_yaw, min_score = std::numeric_limits<float>::max();
             Eigen::Matrix4f min_pose(Eigen::Matrix4f::Identity());
             /* [Part 3] you can perform ICP several times to find a good initial guess */
-			for (yaw = 0; yaw < (M_PI * 2); yaw += 0.05)
-			{
-				Eigen::Translation3f init_translation(gps_point.x, gps_point.y, gps_point.z);
-				Eigen::AngleAxisf init_rotation_z(yaw, Eigen::Vector3f::UnitZ());
-				init_guess = (init_translation * init_rotation_z).matrix();
+            int update_cnt = 0;
+            for (yaw = 1.9; yaw < 2.1; yaw += 0.05)
+            {
+                update_cnt++;
+                // if(update_cnt > 20)
+                //     break;
+                Eigen::Translation3f init_translation(gps_point.x, gps_point.y, gps_point.z);
+                Eigen::AngleAxisf init_rotation_z(yaw, Eigen::Vector3f::UnitZ());
+                init_guess = (init_translation * init_rotation_z).matrix();
                 // std::cout << "Init rotation: \n" << init_rotation_z.matrix() << std::endl;
                 // std::cout << "Init guess of HTMs: \n" << init_guess << std::endl;
 
-				ROS_INFO("---------------------------------------------------------------");
+                ROS_INFO("---------------------------------------------------------------");
 
-				// first_icp.setMaxCorrespondenceDistance(3.2);
-				first_icp.setMaxCorrespondenceDistance(1);
-				first_icp.setMaximumIterations(1000);
-				first_icp.setEuclideanFitnessEpsilon(1e-8);
-				first_icp.setTransformationEpsilon(1e-8);
-				first_icp.setInputSource(filtered_scan_ptr);
-				first_icp.setInputTarget(filtered_map_ptr);
-				ROS_INFO("start align: %f", yaw);
-				// 要對齊的對象是inputSource設定的pointCloud
-				// 被對齊的對象是inputTarget設定的pointCloud
-				// 對齊的結果是transformed_scan_ptr
-				first_icp.align(*transformed_scan_ptr, init_guess);
-				double score = first_icp.getFitnessScore();
-				ROS_INFO("min score: %f, score: %f", min_score, score);
+                // first_icp.setMaxCorrespondenceDistance(3.2);
+                first_icp.setMaxCorrespondenceDistance(2);
+                first_icp.setMaximumIterations(1000);
+                first_icp.setEuclideanFitnessEpsilon(1e-8);
+                first_icp.setTransformationEpsilon(1e-8);
+                first_icp.setInputSource(filtered_scan_ptr);
+                first_icp.setInputTarget(filtered_map_ptr);
+                ROS_INFO("start align: %f", yaw);
+                // 要對齊的對象是inputSource設定的pointCloud
+                // 被對齊的對象是inputTarget設定的pointCloud
+                // 對齊的結果是transformed_scan_ptr
+                first_icp.align(*transformed_scan_ptr, init_guess);
+                double score = first_icp.getFitnessScore();
+                ROS_INFO("min score: %f, score: %f", min_score, score);
 
-				if (score < min_score){
-					min_score = score;
-					// 把icp求出來的轉換關係存下來
-					min_pose = first_icp.getFinalTransformation();
-					ROS_INFO("Update best pose");
-				}
-			}
-			
-			// set initial guess
+                if (score < min_score)
+                {
+                    update_cnt = 0;
+                    min_score = score;
+                    // 把icp求出來的轉換關係存下來
+                    min_pose = first_icp.getFinalTransformation();
+                    ROS_INFO("Update best pose");
+                }
+            }
+            ROS_INFO("Get initial guess");
+            // set initial guess
             init_guess = min_pose;
             initialied = true;
         }
@@ -272,34 +290,34 @@ public:
         /* [Part 2] Perform ICP here or any other scan-matching algorithm */
         /* Refer to https://pointclouds.org/documentation/classpcl_1_1_iterative_closest_point.html#details */
 
-		// icp.setMaxCorrespondenceDistance(1.6);
-		icp.setMaxCorrespondenceDistance(1);
-		icp.setMaximumIterations(1000);
-		icp.setEuclideanFitnessEpsilon(1e-8);
-		icp.setTransformationEpsilon(1e-8);
-		icp.setInputSource(filtered_scan_ptr);
-		icp.setInputTarget(filtered_map_ptr);
-		icp.align(*transformed_scan_ptr, init_guess);
-		result = icp.getFinalTransformation();
-		localization_score += icp.getFitnessScore();
+        // icp.setMaxCorrespondenceDistance(1.6);
+        icp.setMaxCorrespondenceDistance(1);
+        icp.setMaximumIterations(1000);
+        icp.setEuclideanFitnessEpsilon(1e-8);
+        icp.setTransformationEpsilon(1e-8);
+        icp.setInputSource(filtered_scan_ptr);
+        icp.setInputTarget(filtered_map_ptr);
+        icp.align(*transformed_scan_ptr, init_guess);
+        result = icp.getFinalTransformation();
+        localization_score += icp.getFitnessScore();
 
-		/* Use result as next initial guess */
+        /* Use result as next initial guess */
         init_guess = result;
         return result;
     }
 
-    int get_cnt(){
+    int get_cnt()
+    {
         return this->cnt;
     }
 };
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     ros::init(argc, argv, "localizer");
-	ros::NodeHandle n("~");
-	Localizer localizer(n);
-	ros::spin();
-	return 0;
-
+    ros::NodeHandle n("~");
+    Localizer localizer(n);
+    ros::spin();
+    return 0;
 }
